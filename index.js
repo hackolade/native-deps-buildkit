5bf395e4b8bdf2950fa17f5eeb4e5773d0ba2a2e
Ugo Bechameil <ugo.bechameil@hackolade.com>
2023-09-15 14:58:47 +0200

    Refactor publishing packages to npm GH registry Add delete job for published packages

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import {join, resolve} from "node:path";
-import {readdir, writeFile} from "node:fs/promises";
+import {writeFile} from "node:fs/promises";
 import {ROOT_DIR} from "#root";
 import {discoverRegularNativeModules, getElectronAbi} from "#lib/module.js";
 import {installAvailablePrebuilts} from "#lib/install.js";
 import {log} from "#lib/logger.js";
 import {exec, npmCommand} from '#lib/commands.js';
+import { publishToGitHubPackages, prepareParcelWatcherPrebuildsPackages } from '#lib/publish.js';
+
 
 // run patch package
 await exec(
@@ -23,20 +25,26 @@ log('discovered native modules: %O', installedNativeModules);
 const targets = [{targetPlatform: 'darwin', targetArch : 'arm64'}, {targetPlatform: 'darwin', targetArch : 'x64'}, {targetPlatform: 'linux', targetArch : 'x64'}, {targetPlatform: 'win32', targetArch : 'x64'}];
 
 const custom = [];
+const toPublish = await prepareParcelWatcherPrebuildsPackages();
 
 for( const module of installedNativeModules){
     for( const {targetPlatform, targetArch} of targets){
         const installOutput = await installAvailablePrebuilts({module, targetPlatform, targetArch, electron});
         if(installOutput.toBuild){
             custom.push(installOutput);
+        }else{
+            toPublish.push(installOutput.scopedPackagePath);
         }
     }
 }
 
-await writeFile(resolve(ROOT_DIR, 'modulesToBuild.json'), JSON.stringify(custom));
 // write to file as input for other platforms
+await writeFile(resolve(ROOT_DIR, 'modulesToBuild.json'), JSON.stringify(custom));
 
+// await Promise.all(
+//     toPublish.map(async packagePath => publishToGitHubPackages(packagePath))
+// );
 
-
-// trigger other jobs for each platform and upload artifacts
+// then trigger other jobs for each platform and upload artifacts
 //https://docs.github.com/en/actions/using-workflows/storing-workflow-data-as-artifacts
+// This is done as GitHub actions steps
