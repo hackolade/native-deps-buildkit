84e729f885ba2e05f15f81db94156c83346fdc00
Ugo Bechameil <ugo.bechameil@hackolade.com>
2023-09-19 17:29:17 +0200

    Fix typo

--- rebuild.js
+++ rebuild.js
@@ -16,46 +16,45 @@ const modulesToBuild = modules.filter(
 );
 
 for (const { module, targetPlatform, targetArch } of modulesToBuild) {
-    log("building custom native bindings for module %o", module);
-    await prebuildNativeModule(module);
+  log("building custom native bindings for module %o", module);
+  await prebuildNativeModule(module);
 
-    const nativeModuleScopedPackage = path.join(
-      ROOT_DIR,
-      "node_modules",
-      "@hackolade",
-      `${module.name}-${targetPlatform}-${targetArch}`,
-    );
-    await rm(nativeModuleScopedPackage, { force: true, recursive: true });
-    await mkdir(nativeModuleScopedPackage, { force: true, recursive: true });
+  const nativeModuleScopedPackage = path.join(
+    ROOT_DIR,
+    "node_modules",
+    "@hackolade",
+    `${module.name}-${targetPlatform}-${targetArch}`,
+  );
+  await rm(nativeModuleScopedPackage, { force: true, recursive: true });
+  await mkdir(nativeModuleScopedPackage, { force: true, recursive: true });
 
-    const releaseContent = await readdir(
-      path.resolve(module.baseDir, "build", "Release"),
-      {
-        withFileTypes: true,
-      },
-    );
-    const [prebuild] = releaseContent
-      .filter((entry) => entry.isFile() && entry.name.endsWith(".node"))
-      .map((entry) => entry.name);
-    const prebuildSrc = path.join(module.baseDir, "build", "Release", prebuild);
-    await rename(
-      prebuildSrc,
-      path.join(nativeModuleScopedPackage, "prebuild.node"),
-    );
-    await writePkgTpl({
-      moduleName: module.name,
-      targetPlatform,
-      targetArch,
-      scopedPackagePath: nativeModuleScopedPackage,
-      version: module.version,
-    });
-    const ghPackageNpmrc = `//npm.pkg.github.com/:_authToken=\${NODE_AUTH_TOKEN}
-        @hackolade:registry=https://npm.pkg.github.com
-        always-auth=true`;
-    await writeFile(
-      path.join(nativeModuleScopedPackage, ".npmrc"),
-      ghPackageNpmrc,
-    );
-    await exec(npmCommand, ["publish"], { cwd: nativeModuleScopedPackage });
-  }
+  const releaseContent = await readdir(
+    path.resolve(module.baseDir, "build", "Release"),
+    {
+      withFileTypes: true,
+    },
+  );
+  const [prebuild] = releaseContent
+    .filter((entry) => entry.isFile() && entry.name.endsWith(".node"))
+    .map((entry) => entry.name);
+  const prebuildSrc = path.join(module.baseDir, "build", "Release", prebuild);
+  await rename(
+    prebuildSrc,
+    path.join(nativeModuleScopedPackage, "prebuild.node"),
+  );
+  await writePkgTpl({
+    moduleName: module.name,
+    targetPlatform,
+    targetArch,
+    scopedPackagePath: nativeModuleScopedPackage,
+    version: module.version,
+  });
+  const ghPackageNpmrc = `//npm.pkg.github.com/:_authToken=\${NODE_AUTH_TOKEN}
+      @hackolade:registry=https://npm.pkg.github.com
+      always-auth=true`;
+  await writeFile(
+    path.join(nativeModuleScopedPackage, ".npmrc"),
+    ghPackageNpmrc,
+  );
+  await exec(npmCommand, ["publish"], { cwd: nativeModuleScopedPackage });
 }
