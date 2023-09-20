e3dd820901905536d8b6a66c86f72d6e185a089c
Ugo Bechameil <ugo.bechameil@hackolade.com>
2023-09-20 10:06:57 +0200

    Format

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
-import { join, resolve } from "node:path";
-import { writeFile } from "node:fs/promises";
-import { ROOT_DIR } from "#root";
-import { discoverRegularNativeModules, getElectronAbi } from "#lib/module.js";
-import { installAvailablePrebuilts } from "#lib/install.js";
-import { log } from "#lib/logger.js";
-import { exec, npmCommand } from "#lib/commands.js";
+import { join, resolve } from 'node:path';
+import { writeFile } from 'node:fs/promises';
+import { ROOT_DIR } from '#root';
+import { discoverRegularNativeModules, getElectronAbi } from '#lib/module.js';
+import { installAvailablePrebuilts } from '#lib/install.js';
+import { log } from '#lib/logger.js';
+import { exec, npmCommand } from '#lib/commands.js';
 import {
-  publishToGitHubPackages,
-  prepareParcelWatcherPrebuildsPackages,
-  normalizeNativeModulesUnderHackolade,
-} from "#lib/publish.js";
+	publishToGitHubPackages,
+	prepareParcelWatcherPrebuildsPackages,
+	normalizeNativeModulesUnderHackolade,
+} from '#lib/publish.js';
 
 await exec(npmCommand, [
-  "install",
-  "--force",
-  "--no-save",
-  "@parcel/watcher-darwin-arm64@2.3.0",
-  "@parcel/watcher-darwin-x64@2.3.0",
-  "@parcel/watcher-linux-arm64-glibc@2.3.0",
-  "@parcel/watcher-linux-x64-glibc@2.3.0",
-  "@parcel/watcher-win32-x64@2.3.0",
+	'install',
+	'--force',
+	'--no-save',
+	'@parcel/watcher-darwin-arm64@2.3.0',
+	'@parcel/watcher-darwin-x64@2.3.0',
+	'@parcel/watcher-linux-arm64-glibc@2.3.0',
+	'@parcel/watcher-linux-x64-glibc@2.3.0',
+	'@parcel/watcher-win32-x64@2.3.0',
 ]);
 
 // run patch package
-await exec(npmCommand, ["run", `patch-package`]);
+await exec(npmCommand, ['run', `patch-package`]);
 
 const electron = await getElectronAbi();
-const installedNativeModules = await discoverRegularNativeModules(
-  join(ROOT_DIR, "node_modules"),
-);
+const installedNativeModules = await discoverRegularNativeModules(join(ROOT_DIR, 'node_modules'));
 
-log("discovered native modules: %O", installedNativeModules);
+log('discovered native modules: %O', installedNativeModules);
 
 const targets = [
-  { targetPlatform: "darwin", targetArch: "arm64" },
-  { targetPlatform: "darwin", targetArch: "x64" },
-  { targetPlatform: "linux", targetArch: "x64" },
-  { targetPlatform: "win32", targetArch: "x64" },
+	{ targetPlatform: 'darwin', targetArch: 'arm64' },
+	{ targetPlatform: 'darwin', targetArch: 'x64' },
+	{ targetPlatform: 'linux', targetArch: 'x64' },
+	{ targetPlatform: 'win32', targetArch: 'x64' },
 ];
 
 const custom = [];
@@ -46,34 +44,30 @@ const parcelPrebuilds = await prepareParcelWatcherPrebuildsPackages();
 const toPublish = [...parcelPrebuilds, ...customizedNativeModules];
 
 for (const module of installedNativeModules) {
-  for (const { targetPlatform, targetArch } of targets) {
-    const installOutput = await installAvailablePrebuilts({
-      module,
-      targetPlatform,
-      targetArch,
-      electron,
-    });
-    if (installOutput.toBuild) {
-      custom.push(installOutput);
-    } else {
-      toPublish.push(installOutput.scopedPackagePath);
-    }
-  }
+	for (const { targetPlatform, targetArch } of targets) {
+		const installOutput = await installAvailablePrebuilts({
+			module,
+			targetPlatform,
+			targetArch,
+			electron,
+		});
+		if (installOutput.toBuild) {
+			custom.push(installOutput);
+		} else {
+			toPublish.push(installOutput.scopedPackagePath);
+		}
+	}
 }
 
 // write to file as input for other platforms
-await writeFile(
-  resolve(ROOT_DIR, "modulesToBuild.json"),
-  JSON.stringify(custom),
-);
+await writeFile(resolve(ROOT_DIR, 'modulesToBuild.json'), JSON.stringify(custom));
 
 log('publishing packages to internal GitHub registry of Hackolade organization...');
-for(const packagePath of toPublish){ 
-
-  await publishToGitHubPackages(packagePath);
-  log('---> %o published', packagePath);
+for (const packagePath of toPublish) {
+	await publishToGitHubPackages(packagePath);
+	log('---> %o published', packagePath);
 }
- 
+
 // then trigger other jobs for each platform and upload artifacts
 //https://docs.github.com/en/actions/using-workflows/storing-workflow-data-as-artifacts
 // This is done as GitHub actions steps
--- rebuild.js
+++ rebuild.js
@@ -1,63 +1,55 @@
-import { platform, arch } from "node:os";
-import { log } from "#lib/logger.js";
-import { prebuildNativeModule } from "#lib/prebuilds.js";
-import { npmCommand, exec } from "#lib/commands.js";
-import modules from "./modulesToBuild.json" assert { type: "json" };
-import { ROOT_DIR } from "#root";
-import { mkdir, cp, readdir, rm, writeFile } from "node:fs/promises";
-import path from "node:path";
-import { publishToGitHubPackages, writePkgTpl } from "#lib/publish.js";
+import { platform, arch } from 'node:os';
+import { log } from '#lib/logger.js';
+import { prebuildNativeModule } from '#lib/prebuilds.js';
+import { npmCommand, exec } from '#lib/commands.js';
+import modules from './modulesToBuild.json' assert { type: 'json' };
+import { ROOT_DIR } from '#root';
+import { mkdir, cp, readdir, rm, writeFile } from 'node:fs/promises';
+import path from 'node:path';
+import { publishToGitHubPackages, writePkgTpl } from '#lib/publish.js';
 
 // run patch package
-await exec(npmCommand, ["run", `patch-package`]);
+await exec(npmCommand, ['run', `patch-package`]);
 
-const modulesToBuild = modules.filter(
-  (mod) => mod.targetPlatform === platform() && mod.targetArch === arch(),
-);
+const modulesToBuild = modules.filter(mod => mod.targetPlatform === platform() && mod.targetArch === arch());
+
+// Handle our custom module for windows remote desktop detection
+if (os.platform() === 'win32') {
+}
 
 for (const { module, targetPlatform, targetArch } of modulesToBuild) {
+	await rm(path.join(module.baseDir, 'build', 'Release'), { recursive: true, force: true });
 
-  await rm(
-    path.join(module.baseDir, "build", "Release"),
-    {recursive: true, force: true},
-  );
+	log('building custom native bindings for module %o', module);
+	await prebuildNativeModule(module);
 
-  log("building custom native bindings for module %o", module);
-  await prebuildNativeModule(module);
+	const nativeModuleScopedPackage = path.join(
+		ROOT_DIR,
+		'node_modules',
+		'@hackolade',
+		`${module.name}-${targetPlatform}-${targetArch}`,
+	);
+	await rm(nativeModuleScopedPackage, { force: true, recursive: true });
+	await mkdir(nativeModuleScopedPackage, { force: true, recursive: true });
 
-  const nativeModuleScopedPackage = path.join(
-    ROOT_DIR,
-    "node_modules",
-    "@hackolade",
-    `${module.name}-${targetPlatform}-${targetArch}`,
-  );
-  await rm(nativeModuleScopedPackage, { force: true, recursive: true });
-  await mkdir(nativeModuleScopedPackage, { force: true, recursive: true });
+	const releaseContent = await readdir(path.resolve(module.baseDir, 'build', 'Release'), {
+		withFileTypes: true,
+	});
+	const [prebuild] = releaseContent
+		.filter(entry => entry.isFile() && entry.name.endsWith('.node'))
+		.map(entry => entry.name);
 
-  const releaseContent = await readdir(
-    path.resolve(module.baseDir, "build", "Release"),
-    {
-      withFileTypes: true,
-    },
-  );
-  const [prebuild] = releaseContent
-    .filter((entry) => entry.isFile() && entry.name.endsWith(".node"))
-    .map((entry) => entry.name);
-  
-    log("normalizing prebuild name: %o - %o", module.baseDir, prebuild);
-  const prebuildSrc = path.join(module.baseDir, "build", "Release", prebuild);
-  
-  await cp(
-    prebuildSrc,
-    path.join(nativeModuleScopedPackage, "prebuild.node"),
-  );
-  await writePkgTpl({
-    moduleName: module.name,
-    targetPlatform,
-    targetArch,
-    scopedPackagePath: nativeModuleScopedPackage,
-    version: module.version,
-  });
+	log('normalizing prebuild name: %o - %o', module.baseDir, prebuild);
+	const prebuildSrc = path.join(module.baseDir, 'build', 'Release', prebuild);
 
-  await publishToGitHubPackages(nativeModuleScopedPackage);
+	await cp(prebuildSrc, path.join(nativeModuleScopedPackage, 'prebuild.node'));
+	await writePkgTpl({
+		moduleName: module.name,
+		targetPlatform,
+		targetArch,
+		scopedPackagePath: nativeModuleScopedPackage,
+		version: module.version,
+	});
+
+	await publishToGitHubPackages(nativeModuleScopedPackage);
 }
