e8aa5b1eb2c208cf01fb6b2e6372405f6fd7abb8
Ugo Bechameil <ugo.bechameil@hackolade.com>
2023-10-19 09:51:43 +0200

    Fix exec

--- test/install-before-test-run.js
+++ test/install-before-test-run.js
@@ -8,4 +8,6 @@ import { platform } from 'node:os';
 const deps = pkg.map(({rescopedModuleName, version}) => `${rescopedModuleName}@${version}`);
 
 const windowsSpecificDep = platform() === "wind32"? [ "@hackolade/winapi-detect-remote-desktop-addon-win32-x64@22.1.0" ]: [];
-await exec(npmCommand, ['install','--save', 'false', '-w', 'test', ...deps, ...windowsSpecificDep]);
+await exec({
+    command: npmCommand, parameters: ['install','--save', 'false', '-w', 'test', ...deps, ...windowsSpecificDep]
+});
