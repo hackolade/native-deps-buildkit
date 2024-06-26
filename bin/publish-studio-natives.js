dc0be4099b5afe19a4629f573994e5380fddc1f2
Ugo Bechameil <ugo.bechameil@hackolade.com>
2024-06-26 13:48:44 +0200

    Fix injection of dependencies

--- bin/publish-studio-natives.js
+++ bin/publish-studio-natives.js
@@ -4,14 +4,16 @@ import { resolve } from 'node:path';
 import { writeFile } from 'node:fs/promises';
 import { publishToNPM } from '../lib/publish.js';
 
-const deps = pkg.map(({ rescopedModuleName, version }) => `${rescopedModuleName}@${version}`);
+const deps = pkg.map(({ rescopedModuleName, version }) => {
+	return { rescopedModuleName, version };
+});
 
-const windowsSpecificDep = platform() === 'win32' ? ['@hackolade/winapi-detect-rdp@1.1.0'] : [];
+const windowsSpecificDep = [{'@hackolade/winapi-detect-rdp': '1.1.0'}];
 
 const { default: studioNativesPkg } = await import('../studio-natives-modules/package.json', {
 	assert: { type: 'json' },
 });
-studioNativesPkg.dependencies = [...deps, ...windowsSpecificDep];
+studioNativesPkg.dependencies = [...deps, ...windowsSpecificDep].reduce((acc, dep) => {return {...dep, ...acc}}, {});
 
 await writeFile(resolve('./studio-natives-modules/package.json'), JSON.stringify(studioNativesPkg));
 await publishToNPM(resolve('./studio-natives-modules'));
