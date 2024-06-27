b8ea5829cc401da59cd9496db0f00968433c7e38
Ugo Bechameil <ugo.bechameil@hackolade.com>
2024-06-27 11:55:55 +0200

    Fix path and debug

--- bin/publish-studio-natives.js
+++ bin/publish-studio-natives.js
@@ -1,5 +1,4 @@
 import pkg from '../modulesToBuild.json' with { type: 'json' };
-import { platform } from 'node:os';
 import { resolve } from 'node:path';
 import { writeFile } from 'node:fs/promises';
 import { publishToNPM } from '../lib/publish.js';
@@ -10,10 +9,12 @@ const deps = pkg.map(({ name, rescopedModuleName, version }) => {
 
 const windowsSpecificDep = [{'@hackolade/winapi-detect-rdp': '1.1.0'}];
 
-const { default: studioNativesPkg } = await import('../studio-natives-modules/package.json', {
+const { default: studioNativesPkg } = await import('./studio-natives-modules/package.json', {
 	with: { type: 'json' },
 });
 studioNativesPkg.dependencies = [...deps, ...windowsSpecificDep].reduce((acc, dep) => {return {...dep, ...acc}}, {});
 
+
+console.log("- studio native modules pkg:", studioNativesPkg);
 await writeFile(resolve('./studio-natives-modules/package.json'), JSON.stringify(studioNativesPkg));
 await publishToNPM(resolve('./studio-natives-modules'));
