cd47de6da94317a6e15231e8e270779325bab13f
Ugo Bechameil <ugo.bechameil@hackolade.com>
2024-07-12 15:52:22 +0200

    Use assert

--- bin/get-electron-major-version.js
+++ bin/get-electron-major-version.js
@@ -1,4 +1,4 @@
 import semver from 'semver';
-import electron from 'electron/package.json' with { type: 'json' };
+import electron from 'electron/package.json' assert { type: 'json' };
 
 console.log(semver.major(electron.version));
