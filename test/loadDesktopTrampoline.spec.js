bb53e15ddb74a60d14ba486caaf2a17751d35213
Ugo Bechameil <ugo.bechameil@hackolade.com>
2024-10-16 12:04:23 +0200

    Fix desktop trampoline changes

--- test/loadDesktopTrampoline.spec.js
+++ test/loadDesktopTrampoline.spec.js
@@ -3,12 +3,12 @@ import { expect } from 'chai';
 import { stat, access } from 'fs/promises';
 import { constants } from 'fs';
 
-import { getDesktopTrampolinePath, getSSHWrapperPath } from '@hackolade/desktop-trampoline';
+import { getDesktopAskpassTrampolinePath, getSSHWrapperPath } from '@hackolade/desktop-trampoline';
 
 describe('desktop trampoline', async function () {
 	this.timeout(10000);
 
-	const trampolinePath = getDesktopTrampolinePath();
+	const trampolinePath = getDesktopAskpassTrampolinePath();
 	const sshWrapperPath = getSSHWrapperPath();
 
 	const testPathExec = async path => {
